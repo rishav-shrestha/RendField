4ab076ad0b3a170507d96f71d97bfc58cab0a2cc
rishav-shrestha <grish003@edu.xamk.fi>
2026-02-15 17:28:54 +0200

    Player Idle Animation and Player Walking animation Redone and added to the game

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {   
     public float speed = 2.0f;
+    public float jumpforce = 5.0f;
     private Rigidbody2D rb;
     private float horizontalInput;
     private float forwardInput;
@@ -9,22 +10,37 @@ public class PlayerMovement : MonoBehaviour
     public GameObject playerUpperBody;
     public GameObject playerLowerBody;
     public LayerMask groundLayer;
-    public float groundCheckDistance = 0.1f;
+    public float groundCheckRadius = 0.1f;
+    private Collider2D col;
+    private Vector2[] pos = new Vector2[3];
 
     private bool isGrounded;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
- 
+         col = GetComponent<Collider2D>();
     }
     
     // Update is called once per frame
     void Update()
     {
+        pos[0] = new Vector2(
+            col.bounds.min.x+groundCheckRadius+0.03f,
+            col.bounds.min.y
+        );
+        pos[1] = new Vector2(
+            col.bounds.center.x,
+            col.bounds.min.y
+        );
+        pos[2] = new Vector2(
+            col.bounds.max.x-groundCheckRadius-0.03f,
+            col.bounds.min.y
+        );
+        
+        
         //walking
         horizontalInput = Input.GetAxisRaw("Horizontal");
         forwardInput = Input.GetAxisRaw("Vertical");
@@ -57,23 +73,68 @@ public class PlayerMovement : MonoBehaviour
         }
         //Jumping
         CheckGround();
-
         if (isGrounded)
         {
-            Debug.Log("ON GROUND");
+            playerUpperBody.GetComponent<Animator>().SetBool("jumping",false);
+            playerLowerBody.GetComponent<Animator>().SetBool("jumping",false);
         }
-        Debug.DrawRay(transform.position, Vector2.down * groundCheckDistance, Color.red);     
+        else
+        {
+            playerUpperBody.GetComponent<Animator>().SetBool("jumping",true); 
+            playerLowerBody.GetComponent<Animator>().SetBool("jumping",true);
+        }
+        if (isGrounded&&forwardInput > 0)
+        {
+               Jump();
+              
+        }
     }
     void CheckGround()
     {
-        RaycastHit2D hit = Physics2D.Raycast(
-            new Vector2(transform.localScale.x,transform.position.y + transform.localScale.y/2),
-            Vector2.down,
-            groundCheckDistance,
-            groundLayer
-        );
-
-        isGrounded = hit.collider != null;
+        RaycastHit2D[] hit =
+        {
+            Physics2D.CircleCast(
+                pos[0],
+                groundCheckRadius,
+                Vector2.down,
+                groundCheckRadius,
+                groundLayer),
+            Physics2D.CircleCast(
+                pos[1],
+                groundCheckRadius,
+                Vector2.down,
+                groundCheckRadius,
+                groundLayer),
+            Physics2D.CircleCast(
+                pos[2],
+                groundCheckRadius,
+                Vector2.down,
+                groundCheckRadius,
+                groundLayer)
+        };
+        for (int i = 0; i < pos.Length; i++)
+        {
+            isGrounded = hit[i].collider !=null;
+            if (isGrounded)
+            {
+                return;
+            }
+        }
+      
     }
-  
+    void OnDrawGizmosSelected()
+    {
+        
+        if (!col) col = GetComponent<Collider2D>();
+        Gizmos.color = Color.yellow;
+        for (int i = 0; i < pos.Length; i++)
+        {
+            Gizmos.DrawWireSphere(pos[i], groundCheckRadius); 
+        }
+       
+    }
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+    }
 }
